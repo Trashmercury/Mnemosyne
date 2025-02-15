c120efffcd7b0f0152a57caf1f9c72c9c7528cf8
Trashmercury <trashsh25@gmail.com>
2025-02-14 21:07:54 -0300

    boa noite

---
+++ sandbox/condicionais.js
@@ -0,0 +1,126 @@
+//exercicio1
+//let num = prompt("digite um número:")
+
+//if (num > 0) {
+
+//    console.log("Positivo.");
+
+//} else if (num < o) {
+
+//   console.log("Negativo.");
+
+//} else {
+
+//    console.log("É zero.");
+
+//}
+//exercicio2
+//let num = prompt("Digite um número");
+
+//if (num % 2 == 0) {
+
+//  console.log("É par.")
+
+//} else {
+
+//  console.log("É impar.")
+
+//}
+//exercicio3
+//let idade = prompt("Digite sua idade");
+
+//if (idade >= 0 && idade <=12) {
+
+ // console.log("Você é criança");
+
+//}
+
+//else if (idade >= 13 && idade <= 17) {
+
+ // console.log("Você é adolescente");
+
+//}
+
+//else if (idade >=18) { 
+
+ // console.log("Você é adulto");
+
+//}
+
+//else {
+
+//  console.log("idade invalida")
+
+//}
+
+//exercicio4
+//let nota = prompt("Digite a nota");
+
+//if (nota >= 60) {
+
+//  console.log("Aprovado");
+
+//} else if (nota <= 59) { 
+
+//  console.log("Reprovado");
+
+//}
+
+//exercicio3 intermediario
+
+//let valor = prompt("Digite o valor");
+
+//if (valor >= 100) {
+
+ // console.log("Você ganhou 10% de desconto, total compra 90$")
+
+//} else {
+
+// console.log("$" , valor , "Valor sem desconto");
+
+//}
+
+//exercicio4 intermediario
+//let usuario = prompt("Digite seu usuario");
+//let senha = prompt("Digite sua senha")
+
+//if (usuario == "admin" && senha == 1234) {
+
+//  console.log("Acesso bem sucedido!");
+
+//} else {
+
+// console.log("Acesso negado");
+
+//}
+
+//exercicio3 avançado
+//let nota = prompt("Digite a nota");
+
+//if (nota >= 90 && nota <= 100) {
+
+ // console.log("Nota A");
+
+//} else if (nota >= 80 && nota <= 89) {
+
+  //console.log("Nota B");
+
+//} else if (nota >= 70 && nota <= 79) {
+
+ // console.log("Nota C");
+
+//} else if (nota >= 60 && nota <= 69) {
+
+//  console.log("Nota D");
+
+//} else if (nota <= 59) {
+
+ // console.log("Nota F");
+
+//}
+//exercicio2 intermediario
+let num = prompt("Digite o primeiro numero");
+let num2 = prompt("DIgite o segundo numero");
+let num3 = prompt("Digite o terceiro numero");
+
+
---
+++ sandbox/script.js
@@ -0,0 +1,51 @@
+console.log("Hello Word");
+// Variáveis
+// Atividade 1
+// Exercício 1 e 2
+var idade = 22;
+var idade = "vinte e dois";
+console.log(idade);
+let nome = "Ariel";
+nome = "EduardoS";
+nome = "Patrick";
+console.log(nome);
+const cargo = "professor";
+console.log(cargo);
+// Exercío 3
+const nome_ = "Ariel";
+let idade_ = 22;
+let mensagem = `Meu nome é ${nome_}, tenho ${idade_} anos.`; // Interpolação
+let mensage = "Meu nome é" + nome + ",tenho " + idade + " anos."; // Concatenação
+console.log(mensagem);
+// Tipos Primitivos
+// Atividade 2
+// Exercício 1
+console.log("Hello Word");
+let altura = 12;
+console.log(altura);
+let maiorDeIdade = true;
+let temFilhos = false;
+let tamanho;
+console.log(tamanho);
+let vazio = null
+// Exercício 2
+console.log(typeof nome_);
+console.log(typeof idade_);
+// Exercício 3
+let a = 10;
+let b = "Ariel";
+let c = "10";
+let d = 20;
+console.log("a + b", a + b);
+console.log("a + c", a + c);
+console.log("a + d", a + d);
+// Conversões de tipos
+// Atividade 3
+// Exercício 1 e 2
+console.log("5" + 3);  // "5
+console.log("5" - 3);  // 2
+let n1 = "42.5123";
+n1 = Number(n1);
+console.log(n1);
+console.log(parseInt(n1));
+console.log(parseFloat("42"));
